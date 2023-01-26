b7d7fd31b8fc8addc938aada747d1a5e1a749e64
Matthew <75327868+matthlmes@users.noreply.github.com>
2023-01-26 01:05:23 +0000

    Add files via upload

---
+++ Lab 01 - Week 1/DoubleArithmetic/src/doublearithmetic/DoubleArithmetic.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package doublearithmetic;
+
+/**
+ *
+ * @author matt2
+ */
+public class DoubleArithmetic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double a = 1.5;
+        double b = 2.5;
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+    
+}
